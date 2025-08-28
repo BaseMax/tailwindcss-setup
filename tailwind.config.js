eea1f2afab20f5fd60c03221f0e7f6d0345ea835
Max Base <MaxBaseCode@gmail.com>
2025-08-28 19:07:21 +0330

    Fix and fully test, now i have custom colors to support tailwindcss 4, not 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html', './*.js'],
+  content: [
+    './*.html',
+    './**/*.html',
+    './*.js',
+    './**/*.js'
+  ],
+  safelist: [
+    'bg-table-header',
+    'text-highlight',
+  ],
   theme: {
     extend: {
       colors: {
-        'table-header': '#091a52',
-        'highlight': '#ff5733',
+        // 'table-header': '#091a52',
+        // 'highlight': '#ff5733',
       },
     },
   },
-  safelist: ['bg-table-header', 'text-highlight'],
   plugins: [],
 };
